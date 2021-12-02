031618bb8eb08a863a70c7221f3b0f97eb7e7285
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2021-12-02 10:33:43 -0500

    day 1 solution

--- 2021/src/day_1.jl
+++ 2021/src/day_1.jl
@@ -1,14 +1,17 @@
 # https://adventofcode.com/2021/day/1
 using AdventOfCode
 
-input = readlines("2021/data/day_1.txt")
+input = parse.(Int, readlines("2021/data/day_1.txt"))
 
 function part_1(input)
-    nothing
+    comparisons = @views input[1:end-1] .< input[2:end]
+    return sum(comparisons)
 end
+
 @info part_1(input)
 
 function part_2(input)
-    nothing
+    @views part_1(input[1:end-2] .+ input[2:end-1] .+ input[3:end])
 end
+
 @info part_2(input)
